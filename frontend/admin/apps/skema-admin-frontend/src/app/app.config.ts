8397a7b1cd7779d2f0b45410d918f132b36ff305
Ihcaer <jakub2002410@wp.pl>
2025-11-20 17:04:48 +0100

    feat: add and configure primeng with tailwind

--- frontend/admin/apps/skema-admin-frontend/src/app/app.config.ts
+++ frontend/admin/apps/skema-admin-frontend/src/app/app.config.ts
@@ -5,11 +5,20 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { DefaultPreset } from '@themes/default-theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: DefaultPreset,
+      },
+    }),
   ],
 };
---
+++ frontend/admin/libs/ui/theme/default-theme/README.md
@@ -0,0 +1,7 @@
+# default-theme
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test default-theme` to execute the unit tests.
