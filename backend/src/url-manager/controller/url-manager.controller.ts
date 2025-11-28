28a9acba9799bcd7f90c5eb83969c322b251a77b
Ihcaer <jakub2002410@wp.pl>
2025-11-28 17:20:25 +0100

    refactor: correct typos

--- backend/src/url-manager/controller/url-manager.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { UrlManagerService } from '@url-manager/service/url-manager.service';
-
-@Controller('url-manager')
-export class UrlManagerController {
-  constructor(private readonly urlManagerService: UrlManagerService) { }
-}
